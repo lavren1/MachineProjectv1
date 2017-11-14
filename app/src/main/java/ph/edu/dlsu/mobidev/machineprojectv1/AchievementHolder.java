478483af70fab0352d8a8fbfb1fd49d336ec6a00
NeilTyson <aguilavalor@yahoo.com>
2017-11-15 02:42:35 +0800

    Add files via upload

---
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/AchievementHolder.java
@@ -0,0 +1,45 @@
+package com.example.mobidev.machineproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by Nikko on 11/12/2017.
+ */
+
+public class AchievementHolder extends ViewHolder {
+    private final TextView tvAchievementTitle;
+    private final TextView tvAchievementDesc;
+    private final TextView tvAchievementDateTime;
+    public final Button btnDeleteAchievement;
+
+    public AchievementHolder(View itemView) {
+        super(itemView);
+
+        tvAchievementTitle = itemView.findViewById(R.id.tv_achievement_title);
+        tvAchievementDesc = itemView.findViewById(R.id.tv_achievement_desc);
+        tvAchievementDateTime = itemView.findViewById(R.id.tv_achievement_ts);
+        btnDeleteAchievement = itemView.findViewById(R.id.btn_delete_achievement);
+    }
+
+    public void setTitle (String t){
+        tvAchievementTitle.setText(t);
+    }
+
+    public void setDesc (String t){
+        tvAchievementDesc.setText(t);
+    }
+
+    public void setTimestamp (Timestamp ts){
+        Date date = new Date(ts.getTime());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        tvAchievementDateTime.setText(sdf.format(date));
+    }
+}
---
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/AchievementsFragment.java
@@ -0,0 +1,109 @@
+package com.example.mobidev.machineproject;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.Array;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Nikko on 11/11/2017.
+ */
+
+public class AchievementsFragment extends Fragment {
+    private static final String TAG = "FeedsFragment";
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mFirebaseDB;
+    private RecyclerView rvAchievements;
+    private FloatingActionButton fabAddAchievement;
+    EditText etAchieveTitle, etAchieveDescription;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.achievements, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mFirebaseDB = FirebaseDatabase.getInstance().getReference("achievements");
+
+        rvAchievements = view.findViewById(R.id.self_achievements);
+        rvAchievements.setLayoutManager(new LinearLayoutManager(getContext().getApplicationContext()));
+
+        etAchieveTitle = (EditText) view.findViewById(R.id.form_achTitle);
+        etAchieveDescription = (EditText) view.findViewById(R.id.form_achDesc);
+        fabAddAchievement = (FloatingActionButton) view.findViewById(R.id.fab_add_achievement);
+
+        //firebase recycler view
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child(user.getUid()).child("achievements");
+        FirebaseRecyclerAdapter<Achievement, AchievementHolder>achvmntAdapter = new FirebaseRecyclerAdapter<Achievement, AchievementHolder>(Achievement.class, R.layout.achievement_instance, AchievementHolder.class, ref){
+            @Override
+            protected void populateViewHolder(AchievementHolder viewHolder, Achievement model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDescription());
+                viewHolder.setTimestamp(model.getTimestamp());
+                final String achievementID = model.getAchievementId();
+
+                viewHolder.btnDeleteAchievement.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        deleteAchievement(achievementID);
+                    }
+                });
+            }
+        };
+        rvAchievements.setAdapter(achvmntAdapter);
+
+        fabAddAchievement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddAchievementDialog dialog1 = new AddAchievementDialog();
+                dialog1.show(getFragmentManager(), "addachievement");
+            }
+        });
+
+        return view;
+    }
+
+    protected void deleteAchievement(String achievementID){
+        FirebaseUser cUser = mAuth.getCurrentUser();
+        FirebaseDatabase ref = FirebaseDatabase.getInstance();
+        DatabaseReference achvmntRef = FirebaseDatabase.getInstance().getReference("achievements").child(achievementID);
+        DatabaseReference userAchvmntRef = FirebaseDatabase.getInstance().getReference("users").child(cUser.getUid()).child("achievements").child(achievementID);
+        achvmntRef.removeValue();
+        userAchvmntRef.removeValue();
+    }
+}

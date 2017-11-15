e874ae4bebf1fbe1dbf4f8a0adbfc2822036181e
NeilTyson <aguilavalor@yahoo.com>
2017-11-15 15:56:23 +0800

    THE SPANISH INQUISITION

---
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedHolder.java
@@ -0,0 +1,46 @@
+package com.example.mobidev.machineproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by Nikko on 11/15/2017.
+ */
+
+public class FeedHolder extends RecyclerView.ViewHolder {
+
+    private final TextView tvFeedAchievementOwner;
+    private final TextView tvFeedAchievementTitle;
+    private final TextView tvFeedAchievementDesc;
+    private final TextView tvFeedAchievementDateTime;
+
+    public FeedHolder(View itemView) {
+        super(itemView);
+
+        tvFeedAchievementOwner = itemView.findViewById(R.id.tv_feedAchievement_owner);
+        tvFeedAchievementTitle = itemView.findViewById(R.id.tv_feedAchievement_title);
+        tvFeedAchievementDesc = itemView.findViewById(R.id.tv_feedAchievement_desc);
+        tvFeedAchievementDateTime = itemView.findViewById(R.id.tv_feedAchievement_ts);
+    }
+
+    public void setOwner (String t) { tvFeedAchievementOwner.setText(t); }
+
+    public void setTitle (String t){
+        tvFeedAchievementTitle.setText(t);
+    }
+
+    public void setDesc (String t){
+        tvFeedAchievementDesc.setText(t);
+    }
+
+    public void setTimestamp (java.sql.Timestamp ts){
+        Date date = new Date(ts.getTime());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        tvFeedAchievementDateTime.setText(sdf.format(date));
+    }
+}
--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedsFragment.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedsFragment.java
@@ -1,15 +1,17 @@
-package ph.edu.dlsu.mobidev.machineprojectv1;
+package com.example.mobidev.machineproject;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -20,10 +22,14 @@ import com.google.firebase.database.FirebaseDatabase;
  * Created by Nikko on 11/11/2017.
  */
 
-public class FeedsFragment extends Fragment implements View.OnClickListener{
+public class FeedsFragment extends Fragment {
     private static final String TAG = "FeedsFragment";
 
     private FirebaseAuth mAuth;
+    Button logoutbutton;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mFirebaseDB;
+    private RecyclerView rvFeed;
 
     @Nullable
     @Override
@@ -31,32 +37,37 @@ public class FeedsFragment extends Fragment implements View.OnClickListener{
         View view = inflater.inflate(R.layout.feed, container, false);
 
         mAuth = FirebaseAuth.getInstance();
-
         FirebaseUser user = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mFirebaseDB = FirebaseDatabase.getInstance().getReference("achievements");
 
+        rvFeed = (RecyclerView) view.findViewById(R.id.world_feed);
+        rvFeed.setLayoutManager(new LinearLayoutManager(getContext().getApplicationContext()));
+
+        //firebase recycler view
+        DatabaseReference allAchRef = FirebaseDatabase.getInstance().getReference("achievements");
+        FirebaseRecyclerAdapter<Achievement, FeedHolder> feedAdapter = new FirebaseRecyclerAdapter<Achievement, FeedHolder>(Achievement.class, R.layout.feed_achievement_display, FeedHolder.class, allAchRef){
+            @Override
+            protected void populateViewHolder(FeedHolder viewHolder, Achievement model, int position) {
+                viewHolder.setOwner(model.getUsername());
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setDesc(model.getDescription());
+                viewHolder.setTimestamp(model.getTimestamp());
+            }
+        };
+        rvFeed.setAdapter(feedAdapter);
+
+        logoutbutton = (Button) view.findViewById(R.id.logout_feed);
+        logoutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent gobacktosquareone = new Intent(getActivity(), LoginActivity.class);
+                startActivity(gobacktosquareone);
+                getActivity().finish();
+            }
+        });
 
         return view;
-    }
-
-    public void deleteAllData(){ //for dev
-        DatabaseReference usernode = FirebaseDatabase.getInstance().getReference().getRoot().child("users");
-        DatabaseReference goalnode = FirebaseDatabase.getInstance().getReference().getRoot().child("goals");
-
-        usernode.setValue(null);
-        goalnode.setValue(null);
-    }
-
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.btn_log_out:
-                mAuth.signOut();
-                startActivity(new Intent(getContext().getApplicationContext(), LoginActivity.class));
-                break;
-            case R.id.btn_delete_all:
-                deleteAllData();
-                break;
-        }
-
     }
 }

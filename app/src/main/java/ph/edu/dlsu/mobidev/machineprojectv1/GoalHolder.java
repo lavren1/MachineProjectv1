9225907cfa75f8491fa3b3ea20c776ae8dcbb862
NeilTyson <aguilavalor@yahoo.com>
2017-12-13 23:28:38 +0800

    added btnAchieveGoal

--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/GoalHolder.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/GoalHolder.java
@@ -19,6 +19,7 @@ public class GoalHolder extends ViewHolder{
     private final  TextView tvGoalDesc;
     private final TextView tvGoalDateTime;
     public final Button btnEditGoal;
+    public final Button btnAchieveGoal;
 
     public GoalHolder(View itemView) {
         super(itemView);
@@ -27,6 +28,7 @@ public class GoalHolder extends ViewHolder{
         tvGoalDesc = itemView.findViewById(R.id.tv_goal_desc);
         tvGoalDateTime = itemView.findViewById(R.id.tv_goal_ts);
         btnEditGoal = itemView.findViewById(R.id.btn_edit_goal);
+        btnAchieveGoal = itemView.findViewById(R.id.btn_achieve_goal);
 
     }
 

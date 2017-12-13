53d8907db5f8ece3880adb237569d9ff8fd7acdf
NeilTyson <aguilavalor@yahoo.com>
2017-12-13 23:25:06 +0800

    added achieve goal method

--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/GoalsFragment.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/GoalsFragment.java
@@ -236,5 +236,27 @@ public class GoalsFragment extends Fragment implements View.OnClickListener{
             showSnackbar("Edited Goal");
         }
     }
+    
+    protected void achieveGoal (Goal model) {
+        Achievement newModel = new Achievement();
+        ph.edu.dlsu.mobidev.machineprojectv1.Timestamp ts = new ph.edu.dlsu.mobidev.machineprojectv1.Timestamp(System.currentTimeMillis());
+
+        newModel.setTitle(model.getTitle());
+        newModel.setDescription(model.getDescription());
+        newModel.setTimestamp(ts);
+        newModel.setUsername(model.getUsername());
+        newModel.setAchievementId(model.getGoalId());
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference achvmntRef = FirebaseDatabase.getInstance().getReference("achievements").child(newModel.getAchievementId());
+        achvmntRef.setValue(newModel);
+        DatabaseReference userAchvmntRef = FirebaseDatabase.getInstance().getReference("users").child(currentUser.getUid()).child("achievements").child(newModel.getAchievementId());
+        userAchvmntRef.setValue(newModel);
+
+        DatabaseReference goalRef = FirebaseDatabase.getInstance().getReference("users").child(currentUser.getUid()).child("goals").child(model.getGoalId());
+        goalRef.removeValue();
+
+        showSnackbar("Goal achieved!");
+    }
 
 }

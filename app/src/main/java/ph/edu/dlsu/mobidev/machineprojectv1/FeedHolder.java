b4eb2be63f9c3cc1249774219d1ed4100c1a9511
NeilTyson <aguilavalor@yahoo.com>
2017-12-14 19:20:20 +0800

    Update FeedHolder.java

--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedHolder.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedHolder.java
@@ -57,7 +57,7 @@ public class FeedHolder extends RecyclerView.ViewHolder {
 
     public void setTimestamp (java.sql.Timestamp ts){
         Date date = new Date(ts.getTime());
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM d, yyyy 'at' hh:mm aaa");
         //TODO fix format
         tvFeedAchievementDateTime.setText(sdf.format(date));
     }

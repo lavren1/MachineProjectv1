8c64d46316fa6280f7b1169c3e8ec4ea4e35a9d1
MylesAlveniz <myles.alveniz@yahoo.com>
2017-12-15 02:14:38 +0800

    Design: Buttons and HR

--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/AchievementsFragment.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/AchievementsFragment.java
@@ -1,5 +1,6 @@
 package ph.edu.dlsu.mobidev.machineprojectv1;
 
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;

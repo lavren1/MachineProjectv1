c028053b9638723382370e2776698eec1801d9f5
NeilTyson <aguilavalor@yahoo.com>
2017-12-16 15:16:25 +0800

    Update FeedsFragment.java

--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedsFragment.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/FeedsFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;

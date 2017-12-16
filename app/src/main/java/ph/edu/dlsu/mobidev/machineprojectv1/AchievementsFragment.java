a5df530e30bb2eeb13aeef5caddc55e288824b5a
NeilTyson <aguilavalor@yahoo.com>
2017-12-16 15:16:04 +0800

    Update AchievementsFragment.java

--- app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/AchievementsFragment.java
+++ app/src/main/java/ph/edu/dlsu/mobidev/machineprojectv1/AchievementsFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
